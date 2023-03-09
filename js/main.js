6a324b0b67f7d8dc9fce045f32f62fab347076a8
vee180 <venerasultann@gmail.com>
2023-03-09 15:23:57 -0500

    Finished JS

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ console.log("Js test main branch");
 
 //make  random words work on the screen 
 let randomWords = ['candy', 'nature', 'bean', 'bear', 'water', 'red', 'rose', 'spring', 'blue', 'power', 'car', 'pen', 'ear', 'laugh', 'plate', 'house', 'party', 'salt', 'shop', 'phone'];
+
 let randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
 console.log(randomWord);
+
+
 
 //find an indexof letter whatever player clicked
 let secretWord = document.getElementById("secretWord");
@@ -23,7 +26,7 @@ pEl.style.display = 'none';
 console.log(pEl)
 
 
-//letter buttons will disappear after pressing it one time 
+//letter buttons should disappear after pressing it one time 
 function setLetter(event) {
     let btn = event.target
     btn.onclick = function () {
@@ -46,8 +49,6 @@ letter.addEventListener('click', (evt) => {
         evt.target.style.backgroundColor = 'green';
     }
     else {
-        let changeImg = document.getElementById("hangman")
-        changeImg.style.marginTop = "-310px";
         evt.target.style.backgroundColor = "red";
         numberOfTries += 1;
     }
@@ -56,18 +57,6 @@ letter.addEventListener('click', (evt) => {
     console.log(displayMessage()); //calling the function
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 //if player guessed the hidden word correct then display something
 //  player couldnt guess it then move your img to up and player has 2 chances, if player loses it display something else;
